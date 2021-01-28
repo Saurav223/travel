8582170a156f12e77c041ff967b149fced30769d
saurav <antoneyosaurav111@gmail.com>
2021-01-28 15:18:03 +0545

    first

---
+++ projectOne.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    document.querySelector(".body").style.padding = "1rem";
+    document.querySelector(".centre").style.display = "none";
+    document.getElementById("remove").classList.remove("display");
+}, 3000)
